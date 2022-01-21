d5a6d66dbc2f29e7fa7ac521d75a7a79decd9ee9
peterchege <peterchege442@gmail.com>
2022-01-21 19:16:47 +0300

    loops

---
+++ src/com/peterchege/Loops.java
@@ -0,0 +1,21 @@
+package com.peterchege;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals("quit")){
+            System.out.print("Input:");
+            input = scanner.next().toLowerCase();
+            if(input.equals("quit"))
+                break;
+                System.out.println(input);
+
+        }
+
+    }
+}
