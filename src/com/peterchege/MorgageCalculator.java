d7a0d0727cb672531a4961d39c038718a5f132c3
peterchege <peterchege442@gmail.com>
2022-01-21 19:38:39 +0300

    adding validations to input fields

--- src/com/peterchege/MorgageCalculator.java
+++ src/com/peterchege/MorgageCalculator.java
@@ -8,19 +8,41 @@ public class MorgageCalculator {
         final byte MONTH_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        double monthlyIntrest = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Principal:");
-        int principal = scanner.nextInt();
+        while(true){
+            System.out.print("Enter Principal:");
+            principal = scanner.nextInt();
+            if(principal >= 1_000 && principal <= 1_000_000)
+                break;
+                System.out.println("Enter a value between 1K to 1M");
+        }
 
-        System.out.print("Enter Annual Interest Rate:");
-        float rate = scanner.nextFloat();
-        double monthlyIntrest = rate/PERCENT/MONTH_IN_YEAR;
 
-        System.out.print("Period of years:");
-        byte years = scanner.nextByte();
+        while (true){
+            System.out.print("Enter Annual Interest Rate:");
+            float rate = scanner.nextFloat();
+            if(rate >= 1 && rate <= 30){
+                monthlyIntrest = rate/PERCENT/MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        int numberOfPayments = years * MONTH_IN_YEAR;
+        while(true){
+            System.out.print("Period of years:");
+            byte years = scanner.nextByte();
+            if(years >= 1 && years <= 30){
+                numberOfPayments = years * MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
 
         double firstBrackets = 1 + monthlyIntrest;
         double squareValue = Math.pow(firstBrackets, numberOfPayments);
