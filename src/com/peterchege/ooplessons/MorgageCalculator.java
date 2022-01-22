820d69ba2f7a1d9d2e14daea70f4365e965b9ce0
peterchege <peterchege442@gmail.com>
2022-01-22 19:12:44 +0300

    refactored the morgage calculator function

---
+++ src/com/peterchege/ooplessons/MorgageCalculator.java
@@ -0,0 +1,43 @@
+package com.peterchege.ooplessons;
+
+public class MorgageCalculator {
+    public final static byte MONTHS_IN_YEAR = 12;
+    public final static byte PERCENT = 100;
+
+    private int principal;
+    private  float annualInterest;
+    private byte years;
+
+    public MorgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public double calculateMortgage() {
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public byte getYears() {
+        return years;
+    }
+}
--- src/com/peterchege/ooplessons/MorgageReport.java
+++ src/com/peterchege/ooplessons/MorgageReport.java
@@ -3,22 +3,29 @@ package com.peterchege.ooplessons;
 import java.text.NumberFormat;
 
 public class MorgageReport {
-    public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = Main.calculateMortgage(principal, annualInterest, years);
+
+    private MorgageCalculator calculator;
+
+    public MorgageReport(MorgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= calculator.getYears() * MorgageCalculator.MONTHS_IN_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = Main.calculateBalance(principal, annualInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
     }
 }
