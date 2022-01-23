16eb998effc9841925395f478cac8f2a309c74de
peterchege <peterchege442@gmail.com>
2022-01-23 10:27:17 +0300

    using @override to overwrite methods

--- src/com/peterchege/ooplessons/Main.java
+++ src/com/peterchege/ooplessons/Main.java
@@ -18,6 +18,8 @@ public class Main {
 //        System.out.println(control.isEnabled());
 
         var textBox = new TextBox();
+        textBox.setText("peter chege");
+        System.out.println(textBox);
     }
 
 }
